365fe154346a83a2d5f4cd5bb8b5ee57f55ba584
Tushar Jain <jaintushar731@gmail.com>
2019-07-03 12:02:41 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketIO = require("socket.io");
+
+const publicPath = path.join(__dirname,"../public");
+const port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+app.use(express.static(publicPath));
+
+io.on("connection", (socket)=>{
+	console.log("user was connected");
+	socket.on("disconnect",()=>{
+		console.log("user was disconnect");
+	})
+});
+
+
+server.listen(port,() =>{
+	console.log(`server is up on ${port}`);
+
+});
